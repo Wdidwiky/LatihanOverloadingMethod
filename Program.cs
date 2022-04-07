70c88c27d28f20388c69de48b5577929f2447ef2
wdidwiky <101820999+Wdidwiky@users.noreply.github.com>
2022-04-07 13:37:51 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using LatihanOverloadingMethod;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Minimum #1: {0}", Numbers.FindMinimum(3, 1));
+        Console.WriteLine("Minimum #2: {0}", Numbers.FindMinimum(3, 2, 4));
+        Console.WriteLine();
+        Console.WriteLine("Maximum #1: {0}", Numbers.FindMaximum(3, 1));
+        Console.WriteLine("Maximum $2: {0}", Numbers.FindMaximum(3, 2, 4));
+
+        Console.ReadKey();
+    }
+}
